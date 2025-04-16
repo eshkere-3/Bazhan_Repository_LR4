cdd9393c554379d70c386c0fbabeb7d63a4ff6e2
eshkere-3 <6092listik@gmail.com>
2025-04-16 13:57:30 +0300

    skelet prog

---
+++ Bazhan_project_ LR4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+double enter_A() {
+
+
+}
+
+double enter_B() {
+
+
+}
+
+double proizv(int A, int B) {
+
+
+}
+
+double delen(int A, int B) {
+
+
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    int A=0, B=0;
+    int ch;
+
+    cout << "Меню: " << endl;
+    cout << "1. Ввести А. " << endl;
+    cout << "2. Ввести В. " << endl;
+    cout << "3. Выполнить операцию “*”. " << endl;
+    cout << "4. Выполнить операцию “/”. " << endl;
+
+    cout << "Выберите пункт меню: ";
+
+    return 0;
+
+
+}
+
